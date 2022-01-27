ced682e5868659f244559a7a063bd55fb0086e89
PascalPolygon <pdao2015@my.fit.edu>
2022-01-26 23:04:44 -0500

    Training w LMS

--- HW1/generalizer.py
+++ HW1/generalizer.py
@@ -1,12 +1,23 @@
+from board_utils import BoardUtils
+
+board_utils = BoardUtils()
+
+
 class Generalizer:
     def __init__(self):
         self.w = []
 
-    def LMSWeightUpdate(self, weights, boards, v_trains, eta=0.01):
+    def LMSWeightUpdate(self, weights, boards, v_trains, lr=0.01):
         for b, v_train in zip(boards, v_trains):
-            xs = board_utils.getStateFeatures(b)
+            x1, x2, min_x_to_v, min_o_to_v = board_utils.getStateFeatures(
+                b, expressivity='compact')
+            xs = [x1, x2, min_x_to_v, min_o_to_v]
             v_hat = board_utils.evaluateBoardState(
                 b, weights, expressivity='compact')
-            for i, w in enumerate(weights):
-                w = w + eta(v_train - v_hat)*xs[i]
-        return w
+            for i in range(len(weights)):
+                if i == 0:
+                    weights[i] = weights[i] + lr*(v_train - v_hat)*1
+                else:
+                    # print(xs)
+                    weights[i] = weights[i] + lr*(v_train - v_hat)*xs[i-1]
+        return weights
--- HW1/main.py
+++ HW1/main.py
@@ -4,6 +4,7 @@ from experiment_generator import ExperimentGenerator
 from board_utils import BoardUtils
 from perf_system import PerfSystem
 from critic import Critic
+from generalizer import Generalizer
 import numpy as np
 
 # logging.basicConfig(level=logging.INFO)
@@ -13,68 +14,70 @@ import numpy as np
 
 
 def main():
-    exp_gen = ExperimentGenerator(0.8)
+    exp_gen = ExperimentGenerator(1.0)
     board_utils = BoardUtils()
     perf_system = PerfSystem()
     critic = Critic()
+    generalizer = Generalizer()
+    wins = 0
 
+    for rnd in range(4):
+        print('==================================================')
+        if rnd > 0:
+            print(f'======== ROUND {rnd}: {(wins/rnd)*100}% victories =======')
+        print('==================================================')
     # Determine if opponent is AI or random mover with some probability
-    # p = np.random.uniform(0.0, 1.0)
-    # print(p)
-    # if queues of lost games is not empty
-    if np.random.uniform(0.0, 1.0) > 0.5:
-        # Play AI (self)
-        opponent = 'ai'
-    else:
-        # Play random mover
-        opponent = 'random'
+        if np.random.uniform(0.0, 1.0) > 0.5:
+            # Play AI (self)
+            opponent = 'ai'
+        else:
+            # Play random mover
+            opponent = 'random'
 
-    weights = [0.1, 0.1, 0.1, 0.1, 0.1]
-    b = exp_gen.generateBoard(weights)
-    # print(b)
-    if b is not None:
-        gameTrace = []
-        while(not board_utils.isFinalState(b)):
-            # board_utils.drawBoard(b)
-            print('ego')
-            b = perf_system.play(b, weights, expressivity='compact')
-            board_utils.drawBoard(b)
-            gameTrace.append(b)
-            # opponent = 'ai'
-            if opponent == 'ai':
-                # Replace X's w O's (and vice-versa) and let perf_system play again
-                ob = board_utils.invertBoard(b)
-                print('Playing peer')
-                print(f'Opponent')
-                # board_utils.drawBoard(ob)
-                ob = perf_system.play(ob, weights, expressivity='compact')
-                # board_utils.drawBoard(ob)
-                b = board_utils.invertBoard(ob)
+        weights = [0.1, 0.1, 0.1, 0.1, 0.1]
+        b = exp_gen.generateBoard(weights)
+        # print(b)
+        if b is not None:
+            gameTrace = []
+            while(not board_utils.isFinalState(b)):
+                # board_utils.drawBoard(b)
+                print('ego')
+                b = perf_system.play(b, weights, expressivity='compact')
                 board_utils.drawBoard(b)
-                # gameHistory.append(b)
-            elif opponent == 'random':
-                ob = board_utils.invertBoard(b)
-                print('Playing random')
-                print(f'Opponent')
-                # board_utils.drawBoard(ob)
-                # ob = perf_system.play(ob, weights, expressivity='compact')
-                ob = perf_system.chooseRandomMove(ob)
-                # board_utils.drawBoard(ob)
-                b = board_utils.invertBoard(ob)
-                board_utils.drawBoard(b)
-                # gameHistory.append(b)
+                gameTrace.append(b)
+                # opponent = 'ai'
+                if opponent == 'ai':
+                    # Replace X's w O's (and vice-versa) and let perf_system play again
+                    ob = board_utils.invertBoard(b)
+                    # print('Playing peer')
+                    # print(f'Opponent')
+                    ob = perf_system.play(ob, weights, expressivity='compact')
+                    b = board_utils.invertBoard(ob)
+                    board_utils.drawBoard(b)
+                elif opponent == 'random':
+                    ob = board_utils.invertBoard(b)
+                    ob = perf_system.chooseRandomMove(ob)
+                    b = board_utils.invertBoard(ob)
+                    # board_utils.drawBoard(b)
 
+                print('==================================================')
+
+            print('Game ended')
             print('==================================================')
-
-        print('Game ended')
-        print('==================================================')
-        board_utils.drawBoard(b)
-        # Send game history to citic to genrate training examples
-        boardStates, v_trains = critic.getTrainingExamples(gameTrace, weights)
-        print('Training examples')
-        print('==================================================')
-        board_utils.drawBoard(boardStates[-1])
-        print(v_trains)
+            board_utils.drawBoard(b)
+            if (board_utils.gameWon(b, 1)):
+                wins += 1
+            # Send game history to citic to genrate training examples
+            boardStates, v_trains = critic.getTrainingExamples(
+                gameTrace, weights)
+            print('Training examples')
+            print('==================================================')
+            board_utils.drawBoard(boardStates[-1])
+            # print(v_trains)
+            # Update weights w Generalizer
+            weights = generalizer.LMSWeightUpdate(
+                weights, boardStates, v_trains)
+            print(f'New weights: {weights}')
         # if board_utils.gameWon(b, 1):
         #     print('Won!')
         #     v_train = 100
