898a9df176aaf7e66a5a6a1a55c7d1e9c867a9e6
PascalPolygon <pdao2015@my.fit.edu>
2022-01-27 12:11:16 -0500

    Learning target value function

--- HW1/generalizer.py
+++ HW1/generalizer.py
@@ -8,16 +8,23 @@ class Generalizer:
         self.w = []
 
     def LMSWeightUpdate(self, weights, boards, v_trains, lr=0.01):
+        error = 0
         for b, v_train in zip(boards, v_trains):
             x1, x2, min_x_to_v, min_o_to_v = board_utils.getStateFeatures(
                 b, expressivity='compact')
             xs = [x1, x2, min_x_to_v, min_o_to_v]
             v_hat = board_utils.evaluateBoardState(
                 b, weights, expressivity='compact')
+            print(f'v_train : {v_train} | v_hat : {v_hat}')
+            error += (v_train - v_hat)**2
+            # Update weights
             for i in range(len(weights)):
                 if i == 0:
                     weights[i] = weights[i] + lr*(v_train - v_hat)*1
                 else:
                     # print(xs)
                     weights[i] = weights[i] + lr*(v_train - v_hat)*xs[i-1]
-        return weights
+
+        print(f'error = {error}')
+
+        return weights, error
--- HW1/main.py
+++ HW1/main.py
@@ -20,22 +20,38 @@ def main():
     critic = Critic()
     generalizer = Generalizer()
     wins = 0
+    losses = 0
+    ties = 0
+    errors = []
+    victories = []
 
-    for rnd in range(4):
+    weights = [0.1, 0.1, 0.1, 0.1, 0.1]
+    for rnd in range(50):
         print('==================================================')
         if rnd > 0:
-            print(f'======== ROUND {rnd}: {(wins/rnd)*100}% victories =======')
+            vics = wins/rnd
+            victories.append(vics)
+            print(f'===================== ROUND {rnd} =======================')
+            print(f'============= Victories {vics*100} % ==============')
+            print(
+                f'=============   Losses  {(losses/rnd)*100} % ==============')
+            print(f'=============    Ties   {(ties/rnd)*100} % ==============')
+
         print('==================================================')
     # Determine if opponent is AI or random mover with some probability
-        if np.random.uniform(0.0, 1.0) > 0.5:
+        if np.random.uniform(0.0, 1.0) > 0.3:
             # Play AI (self)
             opponent = 'ai'
         else:
             # Play random mover
             opponent = 'random'
 
-        weights = [0.1, 0.1, 0.1, 0.1, 0.1]
+        # opponent = 'ai'  # Only play self
+        # opponent = 'random'  # Only play random
+        # weights = [0.1, 0.1, 0.1, 0.1, 0.1]
         b = exp_gen.generateBoard(weights)
+        board_utils.drawBoard(b)
+        print(f'Old weights: {weights}')
         # print(b)
         if b is not None:
             gameTrace = []
@@ -43,7 +59,7 @@ def main():
                 # board_utils.drawBoard(b)
                 print('ego')
                 b = perf_system.play(b, weights, expressivity='compact')
-                board_utils.drawBoard(b)
+                # board_utils.drawBoard(b)
                 gameTrace.append(b)
                 # opponent = 'ai'
                 if opponent == 'ai':
@@ -67,6 +83,10 @@ def main():
             board_utils.drawBoard(b)
             if (board_utils.gameWon(b, 1)):
                 wins += 1
+            elif (board_utils.gameWon(b, -1)):
+                losses += 1
+            elif (board_utils.gameTie(b)):
+                ties += 1
             # Send game history to citic to genrate training examples
             boardStates, v_trains = critic.getTrainingExamples(
                 gameTrace, weights)
@@ -75,9 +95,10 @@ def main():
             board_utils.drawBoard(boardStates[-1])
             # print(v_trains)
             # Update weights w Generalizer
-            weights = generalizer.LMSWeightUpdate(
-                weights, boardStates, v_trains)
+            weights, error = generalizer.LMSWeightUpdate(
+                weights, boardStates, v_trains, lr=0.01)
             print(f'New weights: {weights}')
+            errors.append(error)
         # if board_utils.gameWon(b, 1):
         #     print('Won!')
         #     v_train = 100
@@ -87,10 +108,13 @@ def main():
         # elif board_utils.gameTie(b):
         #     print('Tie.')
         #     v_train = 0
-
-        # print('Last board in gameTrace')
-        # print('==================================================')
-        # board_utils.drawBoard(gameTrace[-1])
+    print('ERRORS')
+    print(errors)
+    print('Victories %')
+    print(victories)
+    # print('Last board in gameTrace')
+    # print('==================================================')
+    # board_utils.drawBoard(gameTrace[-1])
 
     # print(b)
     # logger.info(b)
